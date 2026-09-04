9d77682a5e791477584df53e02f4be0a3a61a7c1
Yuri <yuri.rdo@edu.udesc.br>
2026-09-03 21:42:34 -0300

    Exemplo Decisão

--- src/basico/Repeticao.java
+++ src/basico/Repeticao.java
@@ -1,34 +1,34 @@
 package basico;
 
 public class Repeticao {
-   public static void main() {
-       //exemplo for
-     System.out.println("Lista de 1 até 10");
-     for(int i=1; i<=10; i++){
-         System.out.println(i);
-     }
-       System.out.println("FIM");
-       System.out.println("\n\nLista de 10 até 1");
-       //exemplo for decrescente
-     for(int i=10; i>=1; i--){
-         System.out.print(i + " ");
-     }
-       System.out.println("FIM");
-           //versão crescente com while
-           int numero = 1;
-           System.out.println("\n\nLista de 1 até 10 em while:");
-           while (numero <= 10) {
-               System.out.println(numero);
-               numero++;
-           }
-       System.out.println("FIM");
-           //versão descrescente while
-     numero=10;
-       System.out.println("\n\nLista de 10 até 1 em while");
-       while (numero>=1){
-           System.out.println(numero);
-           numero--;
-       }
-       System.out.println("FIM");
-   }
+    public static void main() {
+        //exemplo for
+        System.out.println("Lista de 1 até 10");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("FIM");
+        System.out.println("\n\nLista de 10 até 1");
+        //exemplo for decrescente
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("FIM");
+        //versão crescente com while
+        int numero = 1;
+        System.out.println("\n\nLista de 1 até 10 em while:");
+        while (numero <= 10) {
+            System.out.println(numero);
+            numero++;
+        }
+        System.out.println("FIM");
+        //versão descrescente while
+        numero = 10;
+        System.out.println("\n\nLista de 10 até 1 em while");
+        while (numero >= 1) {
+            System.out.println(numero);
+            numero--;
+        }
+        System.out.println("FIM");
+    }
 }
