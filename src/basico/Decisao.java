5e08736365ee573f03b2e380fb3de87ce293c720
Yuri <yuri.rdo@edu.udesc.br>
2026-09-10 21:46:54 -0300

    Codigos da aula 6

--- src/basico/Decisao.java
+++ src/basico/Decisao.java
@@ -1,8 +1,12 @@
 package basico;
 
+import java.util.Scanner;
+
 public class Decisao {
     public static void main(String[] args) {
-        int nota = 10;
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite uma nota de 0 a 10:\n");
+        int nota = entrada.nextInt();
         if (nota >= 7) {
             System.out.println("Passou direto!");
         } else if (nota >= 4 && nota < 7) {
