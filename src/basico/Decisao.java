f84c8fdd6fea4899f1f1241622c7d4ae3c9d9ec7
Yuri <yuri.rdo@edu.udesc.br>
2026-09-17 20:31:00 -0300

    Aperfeiçoando Decisão

--- src/basico/Decisao.java
+++ src/basico/Decisao.java
@@ -3,15 +3,26 @@ import java.util.Scanner;
 
 public class Decisao {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        System.out.print("Digite uma nota de 0 a 10:\n");
-        int nota = entrada.nextInt();
-        if (nota >= 7) {
-            System.out.println("Passou direto!");
-        } else if (nota >= 4 && nota < 7) {
-            System.out.println("Pegou exame!");
-        } else {
-            System.out.println("Reprovou!");
-        }
+        int nota;
+        do {
+            Scanner entrada = new Scanner(System.in);
+            System.out.print("Digite uma nota de 0 a 10 [-1 para finalizar]:\n");
+            nota = entrada.nextInt();
+            if (nota < -1 || nota > 10){
+                System.out.println("VALOR INVALIDO! TENTE NOVAMENTE!");
+                continue;
+            }
+            if (nota == 1){
+                break;
+            }
+            if (nota >= 7) {
+                System.out.println("Passou direto!");
+            } else if (nota >= 4 && nota < 7) {
+                System.out.println("Pegou exame!");
+            } else{
+                System.out.println("Reprovou!");
+            }
+        } while (nota != -1);
+        System.out.println("Encerrando o sistema");
     }
 }
